b078d87c7d94faf15de3163a40fae7d7759d70f9
formicant <formicant@gmail.com>
2022-06-01 07:33:09 +0700

    Use stack in palindrome generation

--- TokiMonsi.Palindrome/PalindromesGenerator.cs
+++ TokiMonsi.Palindrome/PalindromesGenerator.cs
@@ -22,40 +22,14 @@ public class PalindromesGenerator
 				.Select(words => string.Join(" ", words)))
 			.ToList();
 
-	//IReadOnlyList<IReadOnlyList<string>> GetPalindromicSequences(StartEdge startEdge, int maxWordCount)
-	//{
-	//	var results = new List<IReadOnlyList<string>>();
-	//	AddPalindromicSequencesRecursively(startEdge.ToNode, maxWordCount - 1, ImmutableArray.Create(startEdge.Word), results);
-	//	return results;
-	//}
-
-	//void AddPalindromicSequencesRecursively(Node node, int wordsLeft, ImmutableArray<string> words, IList<IReadOnlyList<string>> result)
-	//{
-	//	if (_graph.Distances.TryGetValue(node, out var distance) && distance <= wordsLeft)
-	//	{
-	//		if (distance == 0)
-	//			result.Add(words);
-
-	//		if (wordsLeft > 0)
-	//			foreach (var edge in _graph.EdgesFromNode[node])
-	//			{
-	//				var newWords = node.Offset >= 0
-	//					? words.Add(edge.Word)
-	//					: words.Insert(0, edge.Word);
-
-	//				AddPalindromicSequencesRecursively(edge.ToNode, wordsLeft - 1, newWords, result);
-	//			}
-	//	}
-	//}
-
 	IEnumerable<IReadOnlyList<string>> GetPalindromicSequences(StartEdge startEdge, int maxWordCount)
 	{
-		var queue = new PriorityQueue<(Node, int, ImmutableArray<string>), int>();
-		queue.Enqueue((startEdge.ToNode, maxWordCount - 1, ImmutableArray.Create(startEdge.Word)), maxWordCount);
+		var stack = new Stack<Position>();
+		stack.Push(new(startEdge.ToNode, maxWordCount - 1, ImmutableArray.Create(startEdge.Word)));
 
-		while (queue.Count > 0)
+		while (stack.Count > 0)
 		{
-			var (node, wordsLeft, words) = queue.Dequeue();
+			var (node, wordsLeft, words) = stack.Pop();
 
 			if (_graph.Distances.TryGetValue(node, out var distance) && distance <= wordsLeft)
 			{
@@ -69,9 +43,15 @@ public class PalindromesGenerator
 							? words.Add(edge.Word)
 							: words.Insert(0, edge.Word);
 
-						queue.Enqueue((edge.ToNode, wordsLeft - 1, newWords), wordsLeft);
+						stack.Push(new(edge.ToNode, wordsLeft - 1, newWords));
 					}
 			}
 		}
 	}
+
+	readonly record struct Position(
+		Node Node,
+		int WordsLeft,
+		ImmutableArray<string> Words
+	);
 }
